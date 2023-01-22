477647b9ba7446df294c80dd0efd84ebabddde2f
scemino <scemino74@gmail.com>
2023-01-22 17:11:50 +0100

    Initial commit

---
+++ libs/sokol/sokol-dll.c
@@ -0,0 +1,12 @@
+#define SOKOL_IMPL
+#define SOKOL_DLL
+#define SOKOL_NO_ENTRY
+#if defined(_WIN32)
+#define SOKOL_LOG(s) OutputDebugStringA(s)
+#endif
+/* sokol 3D-API defines are provided by build options */
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_time.h"
+#include "sokol_audio.h"
+#include "sokol_glue.h"
