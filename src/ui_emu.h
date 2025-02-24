cd881f91dc2a604fa7f373868027814dd983b129
scemino <scemino74@gmail.com>
2025-02-24 20:59:10 +0100

    Add audio UI

--- src/ui_emu.h
+++ src/ui_emu.h
@@ -7,11 +7,20 @@ extern "C" {
 #endif
 
 typedef struct {
+    int             x, y;
+    int             w, h;
+    bool            open;
+    const float*    buffer;  /* pointer to audio sample buffer */
+    int*            num_samples;    /* max number of samples in sample buffer */
+} ui_emu_audio_t;
+
+typedef struct {
     mo5_t* mo5;
 } ui_emu_desc_t;
 
 typedef struct {
     mo5_t*             mo5;
+    ui_emu_audio_t     audio;
     ui_display_t       display;
 } ui_emu_t;
 
@@ -48,6 +57,7 @@ static void _ui_emu_draw_menu(ui_emu_t* ui) {
     EMU_ASSERT(ui && ui->mo5);
     if (ImGui::BeginMainMenuBar()) {
         if (ImGui::BeginMenu("Info")) {
+            ImGui::MenuItem("Audio", 0, &ui->audio.open);
             ImGui::MenuItem("Display", 0, &ui->display.open);
             ImGui::EndMenu();
         }
@@ -56,11 +66,33 @@ static void _ui_emu_draw_menu(ui_emu_t* ui) {
     }
 }
 
+static void _ui_emu_draw_audio(ui_emu_t* ui) {
+    if (!ui->audio.open) {
+        return;
+    }
+    ImGui::SetNextWindowPos(ImVec2((float)ui->audio.x, (float)ui->audio.y), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2((float)ui->audio.w, (float)ui->audio.h), ImGuiCond_FirstUseEver);
+    if (ImGui::Begin("Audio", &ui->audio.open)) {
+        const ImVec2 area = ImGui::GetContentRegionAvail();
+        ImGui::PlotLines("##samples", ui->audio.buffer, *ui->audio.num_samples, 0, 0, -1.0f, +1.0f, area);
+    }
+    ImGui::End();
+}
+
 void ui_emu_init(ui_emu_t* ui, const ui_emu_desc_t* ui_desc) {
     EMU_ASSERT(ui && ui_desc);
     EMU_ASSERT(ui_desc->mo5);
     ui->mo5 = ui_desc->mo5;
     int x = 20, y = 20, dx = 10, dy = 10;
+    {
+        ui->audio.x = x;
+        ui->audio.y = y;
+        ui->audio.w = 200;
+        ui->audio.h = 100;
+        ui->audio.buffer = ui->mo5->audio.buffer;
+        ui->audio.num_samples = &ui->mo5->audio.sample;
+        x += dx; y += dy;
+    }
     {
         ui_display_desc_t desc = {0};
         desc.title = "Display";
@@ -83,17 +115,20 @@ void ui_emu_draw(ui_emu_t* ui, const ui_emu_frame_t* frame) {
     (void)frame;
     EMU_ASSERT(ui && ui->mo5);
     _ui_emu_draw_menu(ui);
+    _ui_emu_draw_audio(ui);
     ui_display_draw(&ui->display, &frame->display);
 }
 
 void ui_emu_save_settings(ui_emu_t* ui, ui_settings_t* settings) {
     EMU_ASSERT(ui && settings);
+    ui_settings_add(settings, "Audio", ui->audio.open);
     ui_settings_add(settings, "Display", ui->display.open);
     ui_display_save_settings(&ui->display, settings);
 }
 
 void ui_emu_load_settings(ui_emu_t* ui, const ui_settings_t* settings) {
     EMU_ASSERT(ui && settings);
+    ui->audio.open = ui_settings_isopen(settings, "Audio");
     ui->display.open = ui_settings_isopen(settings, "Display");
     ui_display_load_settings(&ui->display, settings);
 }
