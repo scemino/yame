aac28d2b5548cf47718bcc83bc309aa381a3e457
scemino <scemino74@gmail.com>
2025-03-11 20:18:10 +0100

    Fix failed to build cpp on Windows

--- src/mo5rom.h
+++ src/mo5rom.h
@@ -3,7 +3,15 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern uint8_t mo5rom[];
 extern uint8_t cd90640rom[];
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif
